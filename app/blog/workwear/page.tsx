18ec10bb872685f20394ca632ff16baa6dda599d
skywaveads22 <skywaveads@hotmail.com>
2025-05-16 18:48:14 +0300

    Delete app directory

--- app/blog/workwear/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function WorkwearRedirect() {
-  // Redirect to the industrial category
-  redirect('/blog/category/industrial')
-  
-  // This won't be rendered, but is needed for the component
-  return null
-} 
